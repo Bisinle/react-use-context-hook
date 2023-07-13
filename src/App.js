236b190805b85075ff5d01133c9f808d0081bc98
Bisinle <Abdiwadud.mohamedd@gmail.com>
2023-07-13 15:10:57 +0300

    implimented useContext Hook and explained how it makes everything simpler

--- src/App.js
+++ src/App.js
@@ -4,10 +4,24 @@ import ComponentC from "./Components/ComponentC";
 
 export const userContext = React.createContext();
 export const schoolContext = React.createContext();
+//STEP1: create the context
+//STEP2: Provide the context-wrap the provide around the component that targets are nested in
+//STEP3: consume the context inside the component that needs it 
+    //inside the component the needs to consume the context
+    //1: import the {useContext} from 'react'
+    //2: import all the contexts you created like the ones above from '..App'
+    //3: call the React useContext package you imported and pass the context
+         //you created as an argument and assign to a variable
+         //const user = userContext(userContext)
+         //const school = userContext(schoolContext)
+
+
 
 function App() {
   return (
     <div className="App">
+
+     
       <userContext.Provider value={"Bisinle"}>
         <schoolContext.Provider value={'Moringa'}>
           <ComponentC />
--- src/Components/ComponentF.js
+++ src/Components/ComponentF.js
@@ -1,28 +1,47 @@
+//THE COMMENTED OUT CODE BELOW IS WHERE WE CAME FROM 
 import React from "react";
+import { useContext } from "react";
 import { userContext, schoolContext } from "../App";
 
 function ComponentF() {
+  //call the userContext and pass the contexts you created as an argument and assign to a variable
+  const user = useContext(userContext);
+  const school = useContext(schoolContext);
   return (
     <div>
-      <userContext.Consumer>
-        {(user) => {
-          return (
-            <schoolContext.Consumer>
-              {(school) => {
-                return (
-                  <>
-                    <h1>User: {user}</h1>
-                    <br />
-                    <h1>School: {school}</h1>
-                  </>
-                );
-              }}
-            </schoolContext.Consumer>
-          );
-        }}
-      </userContext.Consumer>
+      user:{user} - school: {school}
     </div>
   );
 }
 
 export default ComponentF;
+
+
+// import React from "react";
+// import { userContext, schoolContext } from "../App";
+
+// function ComponentF() {
+//   return (
+//     <div>
+//       <userContext.Consumer>
+//         {(user) => {
+//           return (
+//             <schoolContext.Consumer>
+//               {(school) => {
+//                 return (
+//                   <>
+//                     <h1>User: {user}</h1>
+//                     <br />
+//                     <h1>School: {school}</h1>
+//                   </>
+//                 );
+//               }}
+//             </schoolContext.Consumer>
+//           );
+//         }}
+//       </userContext.Consumer>
+//     </div>
+//   );
+// }
+
+// export default ComponentF;
