a8fbc43a9967fe65457f0a6492a412fe2262bb63
Bisinle <Abdiwadud.mohamedd@gmail.com>
2023-07-13 14:58:03 +0300

    Examples of how the context API was used before the useContex HOOK

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import './App.css';
-import ComponentC from './Components/ComponentC'
+import React from "react";
+import "./App.css";
+import ComponentC from "./Components/ComponentC";
 
-
-
-
-export const userContext = React.createContext()
+export const userContext = React.createContext();
+export const schoolContext = React.createContext();
 
 function App() {
   return (
     <div className="App">
-      <userContext.Provider value={'Bisinle'}>
-   <ComponentC/>
+      <userContext.Provider value={"Bisinle"}>
+        <schoolContext.Provider value={'Moringa'}>
+          <ComponentC />
+        </schoolContext.Provider>
       </userContext.Provider>
     </div>
   );
--- src/Components/ComponentF.js
+++ src/Components/ComponentF.js
@@ -1,21 +1,28 @@
-import React from 'react'
-import { userContext } from '../App'
-
+import React from "react";
+import { userContext, schoolContext } from "../App";
 
 function ComponentF() {
   return (
     <div>
-        <userContext.Consumer>
-            {
-                user => {
-                    return <h1>User: {user}</h1>
-                }
-            }
-        </userContext.Consumer>
-        
-        
-        </div>
-  )
+      <userContext.Consumer>
+        {(user) => {
+          return (
+            <schoolContext.Consumer>
+              {(school) => {
+                return (
+                  <>
+                    <h1>User: {user}</h1>
+                    <br />
+                    <h1>School: {school}</h1>
+                  </>
+                );
+              }}
+            </schoolContext.Consumer>
+          );
+        }}
+      </userContext.Consumer>
+    </div>
+  );
 }
 
-export default ComponentF
+export default ComponentF;
